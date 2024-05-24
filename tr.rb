a47b4e48cbe5c95f3a5a913564f6a431a637075e
Santi <s@alvarezrojo.es>
2024-05-25 00:29:11 +0200

    25

--- tr.rb
+++ tr.rb
@@ -2,27 +2,33 @@ require "gnucash"
 
 book = Gnucash.open("/Users/santiagoalvarezrojo/Library/CloudStorage/GoogleDrive-santiago@ecliente.com/Mi\ unidad/gnucash/personal.gnucash")
 
-puts "date;amount;description;account"
+puts "fecha;fondo;#;€;inversión"
 
 book.accounts.each do |account|
-    account.transactions.each do |tx|
-        if tx.description.include?("SCO")
-            puts "#{tx.date.strftime("%d/%m/%Y")};#{tx.value.to_s.gsub(".",",")};##{tx.description};#{account.full_name}"
+    if account.full_name.include?("#SCO")
+        account.transactions.each do |tx|
+            shares = value = 0
+            tx.splits.each do |split|
+                if split[:account].full_name.include?("SCO")
+                    shares = split[:value].to_f
+                else
+                    value = split[:value].to_f
+                end
+            end
+            price = - value / shares
+            puts "#{tx.date.strftime("%d/%m/%Y")};SCO;#{shares.to_s.gsub(".",",")};#{price.to_s.gsub(".",",")};#{value.to_s.gsub(".",",")}"
         end
     end
 end
 
-act = book.find_account_by_full_name("a1) Activos:3. Empresas:#SCO SocialCo")
-balance = Gnucash::Value.zero
-act.transactions.each do |txn|
-    balance += txn.value
-    $stdout.printf("%s  %8s  %8s  %s\n",
-                 txn.date,
-                 txn.value,
-                 balance,
-                 txn.description)
-end
 
-year = Date.today.year
-delta = act.balance_on("#{year}-12-31") - act.balance_on("#{year - 1}-12-31")
-puts "You've saved #{delta} this year so far!"
+
+book.accounts.each do |account|
+    if ["EXPENSE","INCOME"].include?(account.type)
+        account.transactions.each do |tx|
+            if tx.description.include?("SCO")
+                puts "#{tx.date.strftime("%d/%m/%Y")};#{tx.value.to_s.gsub(".",",")};#{tx.description};#{account.full_name};#{account.type}"
+            end
+        end
+    end
+end
