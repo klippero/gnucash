fa98e4ab1835b823427cfc5bf90a69852ae796d2
Santi <s@alvarezrojo.es>
2024-06-29 13:21:32 +0200

    29

--- MiGnucash_test.rb
+++ MiGnucash_test.rb
@@ -66,7 +66,7 @@ class TransactionTest < Minitest::Test
 #        portfolio = Portfolio.new("/Users/santiagoalvarezrojo/Library/CloudStorage/GoogleDrive-santiago@ecliente.com/Mi\ unidad/gnucash/family.gnucash")
         puts
         puts
-#        puts portfolio.to_csv(Date.strptime("11/06/2024","%d/%m/%Y"))
+        puts portfolio.to_csv(Date.strptime("11/06/2024","%d/%m/%Y"))
         puts
         puts
 #        puts portfolio.report_txt(Date.strptime("11/06/2024","%d/%m/%Y"))
